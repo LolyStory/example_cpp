f9e65fccf7321f37756385569762a2cda89c1086
Alexsey Karepov <lolystory777@gmail.com>
2016-09-12 13:37:06 +0300

    add construktor

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 
+
 using namespace std;
+class dodle
+{
+public:
+    int jump;
+    dodle()
+    {
+     jump=0;
+    }
+private:
+
+};
 
 int main(int argc, char *argv[])
 {
+    dodle i;
     cout << "Hello, World!\n";
 
     return 0;
